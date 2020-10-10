1727738b71019bd6032edef0ec15589fdbcf51b3
Aziz Abbas <aziz@builder.io>
2020-10-09 17:26:38 -0700

    minimal reproducts of emotion ssr issue

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,7 +9,11 @@ const IndexPage = () => (
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
     <h1>Hi people</h1>
     <p>Welcome to your new Shop powered by Gatsby and Shopify.</p>
+    <template>
+      <ProductGrid />
+    </template>
     <ProductGrid />
+
     <Link to="/page-2/">Go to page 2</Link>
   </>
 )
